2bf0ff90fe8fa319a6348375cda17217cf6a51fc
SOTA <sota00@outlook.jp>
2021-10-19 14:09:34 -0700

    Update SohtaTestPage.js

--- src/pages/SohtaTestPage.js
+++ src/pages/SohtaTestPage.js
@@ -12,7 +12,7 @@ const SohtaTestPage = () => {
     <div>
       <img src={data[0].sprites.other["official-artwork"].front_default} />
       <p>Name: {data[0].name}</p>
-      <p>Height:{data[0].types.hight}</p>
+      <p>Height:{data[0].height}</p>
       <p>Weight: {data[0].types.weight}</p>
     </div>
   );
